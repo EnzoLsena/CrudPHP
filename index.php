64d4d2d938153276492dc757b1bd4a6be3a3923b
EnzoLsena <enzolevysena@gmail.com>
2024-10-22 17:04:42 -0300

    primeira tela - Done

--- index.php
+++ index.php
@@ -4,12 +4,51 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Document</title>
+    <link href="Bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="res/menu.css" rel="stylesheet">
+
+    <title>Cadastro</title>
 </head>
 
 <body>
+    <div class="container" id="tamanhoContainer">
+        <form>
+
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Número do Produto</label>
+                <input type="number" class="form-control" placeholder="Digite o número do produto">
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Nome do Produto</label>
+                <input type="text" class="form-control" placeholder="Digite o nome do produto">
+            </div>
+            <div class="form-group">
+                <label>Selecione a Categoria</label>
+                <select class="form-control">
+                    <option>Celular</option>
+                    <option>Desktop</option>
+                    <option>Notbook</option>
+                    <option>Hardwares</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Quantidade</label>
+                <input type="number" class="form-control" placeholder="Digite a quantidade do produto">
+            </div>
+
+            <div class="form-group">
+                <label>Fornecedor</label>
+                <select class="form-control">
+                    <option>João fuleiro</option>
+                    <option>Kosko primo</option>
+                    <option>Neca</option>
+
+                </select>
+            </div>
+            <div><button type="button" class="btn btn-success btn-sm">Cadastrar</button></div>
+        </form>
+
+    </div>
 
 
 
@@ -21,10 +60,7 @@
 
 
 
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
+    <script src="Bootstrap/js/bootstrap.js"></script>
 
 </body>
 
